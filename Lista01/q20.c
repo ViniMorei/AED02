ad03eab4d155020387b79a0f73b9583f20d3320b
Vinicius Moreira <2022004046@ifam.edu.br>
2023-04-13 22:16:15 -0400

    Lista 1 de AED02

--- Lista01/q20.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-void entrada(int *vetor[3]){
-    for (int i = 0;i<3;i++){
-        printf("Informe o %d valor: ",i);
-        scanf("%d",vetor[i]);
-    }
-}
